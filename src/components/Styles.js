735afcef2bee7df934f83274eed67f5c5f80a3ed
Luís Henrique <luishsilva09@gmail.com>
2023-07-08 18:31:01 -0300

    feat: gereneric styles

---
+++ src/components/Styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: "#84acd2",
+    width: "90%",
+    height: 60,
+    borderRadius: 29,
+    margin: 20,
+    padding: 10,
+  },
+  imageLogo: {
+    height: 300,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#341C3C",
+  },
+});
+
+export default styles;
