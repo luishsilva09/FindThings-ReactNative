6f02edfbddd9c57bea372817dc5961ec4366adc2
Luís Henrique <luishsilva09@gmail.com>
2023-08-12 18:27:35 -0300

    feat: top bar style

---
+++ src/components/TopBar.js
@@ -0,0 +1,32 @@
+import { View, StyleSheet, Text, Alert } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function TopBar({ navigation }) {
+  return (
+    <View style={styles.content}>
+      <Ionicons
+        name="duplicate-outline"
+        size={32}
+        color="#fff"
+        onPress={() => navigation.navigate("RegisterQR")}
+      />
+      <Ionicons
+        name="exit-outline"
+        size={40}
+        color="#fff"
+        onPress={() => navigation.navigate("Login")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  content: {
+    backgroundColor: "#341C3C",
+    height: 60,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+});
