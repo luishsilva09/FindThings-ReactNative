128a036247cad536bb251cedcc66e9162fe30b47
Luís Henrique <luishsilva09@gmail.com>
2023-08-16 18:18:51 -0300

    fix: clear when logout

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,7 +1,14 @@
-import { View, StyleSheet, Text, Alert, StatusBar } from "react-native";
+import { View, StyleSheet, Text, Alert, StatusBar, Image } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Logo from "../assets/logo.png";
+import stylesGlobal from "../components/Styles";
 
 export default function TopBar({ navigation }) {
+  async function exit() {
+    await AsyncStorage.clear();
+    navigation.navigate("Login");
+  }
   return (
     <View style={styles.content}>
       <StatusBar hidden={false} barStyle={"default"}></StatusBar>
@@ -15,7 +22,7 @@ export default function TopBar({ navigation }) {
         name="exit-outline"
         size={40}
         color="#fff"
-        onPress={() => navigation.navigate("Login")}
+        onPress={() => exit()}
       />
     </View>
   );
